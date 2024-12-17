75a7936ff6a607ae3facda3c8c689459ef6a772f
Hubert <hubertdziadkiewicz00@gmail.com>
2024-12-17 19:49:13 +0100

    Create Bluetooth connection by HC-06 module using SoftwareSerial and create echo to track received Char from mobile application by UART

--- mobile_robot_code/mobile_robot_code.ino
+++ mobile_robot_code/mobile_robot_code.ino
@@ -1,9 +1,24 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <SoftwareSerial.h>
 
+// Inicjalizacja SoftwareSerial (Bluetooth)
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+void setup() {
+    // Rozpocznij komunikację USB z komputerem
+  Serial.begin(9600);
+  // Rozpocznij komunikację Bluetooth
+  mySerial.begin(9600);
+
+
+    // Informacja na monitorze szeregowym
+  Serial.println("Arduino jest gotowe.");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop(){
+    // Odbiór danych z Bluetooth
+  if (mySerial.available() > 0) {
+    char receivedChar = mySerial.read(); // Odczytaj znak
+    Serial.print("Odebrano przez Bluetooth: ");
+    Serial.println(receivedChar); // Wyświetl na monitorze szeregowym
+  }
 }
